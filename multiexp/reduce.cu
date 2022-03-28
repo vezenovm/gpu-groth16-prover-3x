c0fbdebbce334bc894a4b9b3ddc4c3f34859c06f
vezenovm <mvezenov@gmail.com>
2022-03-28 19:36:03 -0400

    change how allocate mem

--- multiexp/reduce.cu
+++ multiexp/reduce.cu
@@ -346,16 +346,16 @@ allocate_memory(size_t nbytes, int dbg = 0) {
 
 var_ptr_async
 allocate_memory_async(size_t nbytes, cudaStream_t &strm, int dbg = 0) {
+    struct VarWithStream *var_async;
     var *mem = nullptr;
-    cudaMallocAsync(&mem, nbytes, strm);
-    if (mem == nullptr) {
+    cudaMallocAsync(&var_async->mem, nbytes, strm);
+    if (var_async->mem == nullptr) {
         fprintf(stderr, "Failed to allocate enough device memory\n");
         abort();
     }
     if (dbg)
         print_meminfo(nbytes);
-    struct VarWithStream *var_async;
-    var_async->mem = mem;
+    // var_async->mem = mem;
     var_async->stream = strm;
     return var_ptr_async(var_async);
 }
