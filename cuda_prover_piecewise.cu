db4b9afaaeb0bdf6825d323a7123cd78411c6417
vezenovm <mvezenov@gmail.com>
2022-04-19 15:35:36 -0400

    missing typo parens for if statement inside chunking

--- cuda_prover_piecewise.cu
+++ cuda_prover_piecewise.cu
@@ -394,7 +394,7 @@ void run_prover(
         printf("w_host + (i * B_m_chunked) * ELT_BYTES: %p\n", w_host + (i * B_m_chunked) * ELT_BYTES);
         printf("w_host + (i * B_m_chunked) * ELT_LIMBS: %p\n", w_host + (i * B_m_chunked) * ELT_LIMBS);
         printf("B_m_chunked * ELT_BYTES: %ld\n", B_m_chunked * ELT_BYTES);
-        if i == CHUNKS - 1 {
+        if (i == CHUNKS - 1) {
             gpuErrchk( cudaMemcpyAsync(w2.get(), w_host2 + ((i * (B_m_chunked - 1)) * ELT_BYTES), B_m_chunked * ELT_BYTES, cudaMemcpyHostToDevice, sB2) ); 
             gpuErrchk( cudaMemcpyAsync(w1.get(), w_host + ((i * (B_m_chunked - 1)) * ELT_BYTES), B_m_chunked * ELT_BYTES, cudaMemcpyHostToDevice, sB1) ); 
         } else {
