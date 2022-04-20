4e9d55415167aae244027b8eb64f66cbcf43d55e
vezenovm <mvezenov@gmail.com>
2022-04-20 15:39:54 -0400

    missed line for comment

--- cuda_prover_piecewise.cu
+++ cuda_prover_piecewise.cu
@@ -460,12 +460,12 @@ void run_prover(
             gpuErrchk( cudaMemcpyAsync(w3.get(), w_host3 + ((2+(i * (L_m_chunked + 1))) * ELT_BYTES), L_m_chunked * ELT_BYTES, cudaMemcpyHostToDevice, sL) ); 
 
         } else {
-            gpuErrchk( 
-                // cudaMemcpyAsync(B1_mults.get(), 
-                // B1_mults_host + get_aff_total_bytes<ECp>(((1U << C) - 1)* i * B_m_chunked), 
-                // get_aff_total_bytes<ECp>(((1U << C) - 1)*B_m_chunked), 
-                // cudaMemcpyHostToDevice, 
-                // sB1) );
+            // gpuErrchk( 
+            //     cudaMemcpyAsync(B1_mults.get(), 
+            //     B1_mults_host + get_aff_total_bytes<ECp>(((1U << C) - 1)* i * B_m_chunked), 
+            //     get_aff_total_bytes<ECp>(((1U << C) - 1)*B_m_chunked), 
+            //     cudaMemcpyHostToDevice, 
+            //     sB1) );
             gpuErrchk( 
                 cudaMemcpyAsync(B1_mults.get(), 
                 B1_mults_host_chunked[i], 
