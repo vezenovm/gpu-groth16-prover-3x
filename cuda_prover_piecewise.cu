6e3576fc87dea059f24999dbe45adb6c21d6a31d
vezenovm <mvezenov@gmail.com>
2022-04-20 21:16:50 -0400

    more debug statements

--- cuda_prover_piecewise.cu
+++ cuda_prover_piecewise.cu
@@ -341,8 +341,10 @@ void run_prover(
                 printf("get_aff_total_bytes<ECp>((B_m_chunks[chunk] * i + k): %ld\n",  get_aff_total_bytes<ECp>((B_m_chunks[chunk] * i) + k));
 
                 // res = B1_mults_host + get_aff_total_bytes<ECp>(curr_row_offset + j + k);
+                printf("get_aff_total_bytes<ECp>(curr_row_offset + j + k): %ld\n",  get_aff_total_bytes<ECp>(curr_row_offset + j + k));
                 void *source = ((char *) B1_mults_host) + get_aff_total_bytes<ECp>(curr_row_offset + j + k);
                 std::memcpy(res, source, get_aff_total_bytes<ECp>(2));
+                printf("completed memcpy: %ld\n",  1);
 
                 // printf("get_aff_total_bytes<ECp>(curr_row_offset + j + k)): %ld\n",  get_aff_total_bytes<ECp>(curr_row_offset + j + k));
 
