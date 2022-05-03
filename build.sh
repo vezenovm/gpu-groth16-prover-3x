bf07f89098e56075a972c37e015c1144f4ed34bb
vezenovm <mvezenov@gmail.com>
2022-05-03 00:24:55 -0400

    change build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 pushd build
-  cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF $EXTRA_CMAKE_ARGS_FOR_CI ..
+  cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-9 -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF $EXTRA_CMAKE_ARGS_FOR_CI ..
   make -j12 main generate_parameters cuda_prover_piecewise
 popd
 mv build/libsnark/main .
